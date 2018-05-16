294c21da6f94990d9dd8692ff14a510257ecd1b7
katyasoup <campbell.katharine@gmail.com>
2018-05-16 15:01:28 -0500

    fix filepaths

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,18 +7,18 @@ var myApp = angular.module('myApp', ['ngRoute']);
         redirectTo: 'home'
       })
       .when('/home', {
-        templateUrl: '/templates/home.html',
+        templateUrl: '/views/templates/home.html',
         controller: 'PitchController as pc',
       })
-    // .when('/info', {
-    //   templateUrl: '../templates/about.html',
-    //   controller: 'InfoController as ic',
-    //   // resolve: {
-    //   //   getuser : function(UserService){
-    //   //     return UserService.getuser();
-    //   //   }
-    //   // }
-    // })
+    .when('/info', {
+      templateUrl: '/views/templates/about.html',
+      controller: 'PitchController as pc',
+      // resolve: {
+      //   getuser : function(UserService){
+      //     return UserService.getuser();
+      //   }
+      // }
+    })
     .otherwise({
       template: '<h1>404</h1>'
     });
