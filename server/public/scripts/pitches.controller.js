9e8bc2d1188a1f6e3de0a5cc25b2d753c8eafd30
katyasoup <campbell.katharine@gmail.com>
2018-05-16 21:56:41 -0500

    add pitches thrown count and avg speed dynamically displayed on dom

--- server/public/scripts/pitches.controller.js
+++ server/public/scripts/pitches.controller.js
@@ -1,23 +1,51 @@
 myApp.controller('PitchController', ['PitchService', function (PitchService) {
-    console.log('PitchController created');
-    var vm = this;
-    vm.pitchService = PitchService;
-    vm.pitchData = PitchService.pitchData;
-    vm.pitchers = {}
-  
-    vm.getPitchers = function () {
-      console.log('getting list of pitcher IDs');
-      PitchService.getPitchers().then(function(response) {
-        vm.pitchers.list = PitchService.pitchers.list;
-        console.log('vm.list', vm.pitchers.list);
-      });
-      
-      
+  console.log('PitchController created');
+  var vm = this;
+  vm.pitchService = PitchService;
+  vm.pitchData = {}
+  vm.pitchers = {}
+  vm.displayStats = false;
+
+  vm.getPitchers = function () {
+    console.log('getting list of pitcher IDs');
+    PitchService.getPitchers().then(function (response) {
+      vm.pitchers.list = PitchService.pitchers.list;
+      console.log('vm.list', vm.pitchers.list);
+    });
+  }
+
+  vm.getPitchDataById = function (id) {
+    console.log('getting pitch data from PitcherID', id);
+    PitchService.getPitchDataById(id).then(function (response) {
+      vm.pitchData.list = PitchService.pitchData.list
+      console.log('LIST', vm.pitchData.list);
+      vm.avgZoneSpeed();
+      vm.displayStats = true;
+    })
+  }
+
+  vm.avgZoneSpeed = function() {
+    let pitchArray = vm.pitchData.list
+    console.log('pitch array', pitchArray);
+
+    let sumSpeed = 0;
+    console.log('zone speed array', pitchArray);
+
+    for (i = 0; i < pitchArray.length; i++) {
+      // console.log('speed is:', pitchArray[i].zonespeed);
+      sumSpeed += parseFloat(pitchArray[i].zonespeed);
+      // console.log('SUM', sumSpeed);
     }
-  
-    vm.getPitchDataById = function (id) {
-        console.log('getting pitch data from PitcherID', id);
-        PitchService.getPitchDataById(id)
-    }
-  
-  }]);
+
+    let avgSpeed = (sumSpeed/pitchArray.length);
+    console.log('AVG', avgSpeed);
+
+    vm.roundedSpeed = Math.round(avgSpeed * 100)/100
+    console.log('rounded:', vm.roundedSpeed);
+    return vm.roundedSpeed;
+  }
+
+  vm.pitchTypes = function() {
+    
+  }
+}]);
--- server/public/scripts/pitches.service.js
+++ server/public/scripts/pitches.service.js
@@ -1,34 +1,27 @@
 myApp.service('PitchService', ['$http', '$location', function ($http, $location) {
-    console.log('PitchService Loaded');
-    var self = this;
-    self.pitchers = {}
-    self.pitchData = {}
-    
+  console.log('PitchService Loaded');
+  var self = this;
+  self.pitchers = {}
+  self.pitchData = {}
 
-    self.getPitchers = function () {
-      // console.log('getting all PitcherIDs');
-      return $http.get('/pitchers').then(function (response) {
-        // console.log('success! here\'s the response:', response);
-        self.pitchers.list = response.data
-        // console.log('array:', self.pitchers.list);
-      })
-    }
-  
-    self.getProfile = function (id) {
-      var thisId = id;
-      return $http.get('/profilesRoute/' + thisId).then(function (response) {
-          self.profile.list = response.data;
-          self.getConnections(thisId);
-      });
-  };
+
+  self.getPitchers = function () {
+    // console.log('getting all PitcherIDs');
+    return $http.get('/pitchers').then(function (response) {
+      // console.log('success! here\'s the response:', response);
+      self.pitchers.list = response.data
+      // console.log('array:', self.pitchers.list);
+    })
+  }
 
 
 
-    self.getPitchDataById = function(id) {
-        console.log('getting pitch data from PitcherID', id);
-        $http.get('/pitchers' + id).then(function(response) {
-            self.pitchData.list = response.data
-            console.log('array:', self.pitchData.list);
-        })
-    }
-  }]);
+
+  self.getPitchDataById = function (id) {
+    console.log('getting pitch data from PitcherID', id);
+    return $http.get('/pitchers/' + id).then(function (response) {
+      self.pitchData.list = response.data
+      console.log('array:', self.pitchData.list);
+    })
+  }
+}]);
